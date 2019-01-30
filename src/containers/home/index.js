ef535c302e70cb60bf1971768c2a90574eec4878
xuxiang <xuxiang@zhujia360.com>
2019-01-30 14:48:46 +0800

    提交

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { Button } from 'antd';
+// import { Button } from 'antd';
 import { REQUEST_ADVERTISINGLIST } from '@/reducers/home';
 import img from '@/assets/yay.jpg'
 export default @connect(state => {return { homeModel:state.home }})
@@ -13,23 +13,23 @@ class Home extends Component {
   requst(){
     const { dispatch } = this.props;
     //console.log(dispatch,'111')
-    dispatch({
-      type:REQUEST_ADVERTISINGLIST,
-      payload:{
-        page:1,
-        rows:10,
-        group_name:"",
-        name:"",
-        group_type:"",
-        is_enable:""
-      }
-    })
+    // dispatch({
+    //   type:REQUEST_ADVERTISINGLIST,
+    //   payload:{
+    //     page:1,
+    //     rows:10,
+    //     group_name:"",
+    //     name:"",
+    //     group_type:"",
+    //     is_enable:""
+    //   }
+    // })
   }
   render() {
     return (
       <div>
         <img src={img} />
-        <Button type="primary" onClick={this.requst.bind(this)}>请求</Button>
+        {/* <Button type="primary" onClick={this.requst.bind(this)}>请求</Button> */}
       </div>
     )
   }
